a671c33060171b81ef23f14826da2863d5534079
Ju-HyeongWoo <gch04319@naver.com>
2022-05-31 10:33:50 +0900

    [fix]쉘스크립트 내용수정

--- dockercompose.sh
+++ dockercompose.sh
@@ -1,4 +1,15 @@
-ecs-cli configure --cluster ec2-tutorial --default-launch-type EC2 --config-name ec2-tutorial --region ap-northeast-2
-# ecs-cli configure profile --access-key FILL_ME_IN --secret-key FILL_ME_IN --profile-name ec2-tutorial-profile
-ecs-cli up --keypair projectju --capability-iam --size 1 --instance-type t2.medium --security-group sg-0e8b01b4716b74cbd --vpc vpc-0fcd911cef7938e9f --subnets subnet-0f694a08b9dd690a2, subnet-07433f10b9575cde5 --cluster-config ec2-tutorial --ecs-profile ec2-tutorial-profile
-ecs-cli compose service up --cluster-config ec2-tutorial --ecs-profile ec2-tutorial-profile
+# 도커 빌드 푸쉬 명령어
+# aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 060701521359.dkr.ecr.ap-northeast-2.amazonaws.com
+# docker build --platform linux/amd64 -t pj4-auth .
+# docker tag pj4-auth:latest 060701521359.dkr.ecr.ap-northeast-2.amazonaws.com/pj4-auth:latest
+# docker push 060701521359.dkr.ecr.ap-northeast-2.amazonaws.com/pj4-auth:latest
+
+#ecs 올리는 명령어 
+# ecs-cli configure --cluster project4-devterr --default-launch-type EC2 --config-name project4-devterr --region ap-northeast-2
+# ecs-cli configure profile --access-key FILL_ME_IN --secret-key FILL_ME_IN --profile-name project4-devterr
+# ecs-cli up --keypair FILL_ME_IN --capability-iam --size 1 --instance-type t2.medium --cluster-config project4-devterr --ecs-profile project4-devterr --vpc FILL_ME_IN --security-group FILL_ME_IN --subnets FILL_ME_IN,FILL_ME_IN
+# ecs-cli compose service up --cluster-config project4-devterr --ecs-profile project4-devterr --target-groups "targetGroupArn=FILL_ME_IN,containerName=node-app,containerPort=3005" --vpc FILL_ME_IN
+
+# 삭제 명령어
+# ecs-cli compose service rm --cluster-config project4-devterr --ecs-profile project4-devterr
+# ecs-cli down --force --cluster-config project4-devterr --ecs-profile project4-devterr

fe3ae261bec92ec6a31a6c7b681b5069a488d668
Ju-HyeongWoo <gch04319@naver.com>
2022-05-31 15:13:43 +0900

    [fix]shellscript workflow 수정

--- dockercompose.sh
+++ dockercompose.sh
@@ -7,8 +7,8 @@
 #ecs 올리는 명령어 
 # ecs-cli configure --cluster project4-devterr --default-launch-type EC2 --config-name project4-devterr --region ap-northeast-2
 # ecs-cli configure profile --access-key FILL_ME_IN --secret-key FILL_ME_IN --profile-name project4-devterr
-# ecs-cli up --keypair FILL_ME_IN --capability-iam --size 1 --instance-type t2.medium --cluster-config project4-devterr --ecs-profile project4-devterr --vpc FILL_ME_IN --security-group FILL_ME_IN --subnets FILL_ME_IN,FILL_ME_IN
-# ecs-cli compose service up --cluster-config project4-devterr --ecs-profile project4-devterr --target-groups "targetGroupArn=FILL_ME_IN,containerName=node-app,containerPort=3005" --vpc FILL_ME_IN
+ecs-cli up --keypair projectju --capability-iam --size 1 --instance-type t2.medium --cluster-config project4-devterr --ecs-profile project4-devterr --vpc vpc-0df90a1ab0fc11825 --security-group sg-07da5f154b06ec072 --subnets subnet-017da35bd76404302,subnet-0f2478bfa3bd03b9b
+ecs-cli compose service up --cluster-config project4-devterr --ecs-profile project4-devterr --target-groups "targetGroupArn=arn:aws:elasticloadbalancing:ap-northeast-2:060701521359:targetgroup/dev-app-dev-tg2/a684b396dda35c01,containerName=node-app,containerPort=3005" --vpc vpc-0df90a1ab0fc11825
 
 # 삭제 명령어
 # ecs-cli compose service rm --cluster-config project4-devterr --ecs-profile project4-devterr
